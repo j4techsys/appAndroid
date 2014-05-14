c0f650458439114ae496155feb4e8f4c8fe9bcdb
031883041848 <031883041848@rmtw022107.tre-mt.gov.br>
2014-05-14 13:02:21 -0400

    testando

---
+++ PassBuss/gen/br/com/j4techsys/passbuss/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.com.j4techsys.passbuss;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PassBuss/src/br/com/j4techsys/passbuss/TesteActivity.java
@@ -0,0 +1,21 @@
+package br.com.j4techsys.passbuss;
+/*
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+public class TesteActivity extends ActionBarActivity {
+	
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_principal);
+		
+		TextView tvInicio = new TextView(this);
+		tvInicio.setText("TESTANDO");
+		setContentView(tvInicio);
+		
+	}
+
+}
+*/
