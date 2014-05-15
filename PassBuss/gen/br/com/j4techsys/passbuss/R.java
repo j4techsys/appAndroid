6647a1f79ae89b8ecc67007c556608fdbedf986c
031883041848 <031883041848@rmtw022107.tre-mt.gov.br>
2014-05-15 11:19:40 -0400

    Enviado dia 15/05/2014 - manhã

--- PassBuss/gen/br/com/j4techsys/passbuss/R.java
+++ PassBuss/gen/br/com/j4techsys/passbuss/R.java
@@ -1091,11 +1091,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050041;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btSalvar=0x7f05003f;
+        public static final int btCadastrar=0x7f05003f;
+        public static final int btCalcular=0x7f05003e;
+        public static final int btConsultar=0x7f050040;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1112,11 +1114,11 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imgLogo=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
-        public static final int nome=0x7f05003e;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
@@ -1139,7 +1141,6 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1244,10 +1245,12 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int salvar=0x7f0a0010;
-        public static final int teste=0x7f0a000e;
+        public static final int cadastrar=0x7f0a0011;
+        public static final int calcular=0x7f0a0010;
+        public static final int consultar=0x7f0a0012;
+        public static final int teste=0x7f0a000f;
     }
     public static final class style {
         /** 
